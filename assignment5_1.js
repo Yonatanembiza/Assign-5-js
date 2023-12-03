e0279e0391415aeeef525829d95a2f2ac7920421
Yonatan Embiza Tadesse <enyonathan@gmail.com>
2023-12-03 14:50:54 -0600

    commit

---
+++ assignment5_1.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function maxOfTwo(input1, input2){
+    if(input1 > input2){
+        return input1;
+    } else if(input1 < input2){
+        return input2;
+    } else {
+        return 0;
+    }
+}
